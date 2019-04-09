c18f8224047b047225d5d31e1221bce43233ee01
Prionti Nasir <pdn3628@rit.edu>
2019-04-08 21:18:51 -0400

    added pattern for no subpaths

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,7 @@ app_name = 'backend'
 
 urlpatterns = [
 
+    path('', views.user_login, name='login'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.user_register, name='register'),
